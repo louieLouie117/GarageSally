0992e1e96d98d223c51231ad4f85e1b840622e61
Kim G <Kim_greenbush@icloud.com>
2021-01-19 15:37:51 -0800

    added models, updated User model

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,15 +10,20 @@ namespace UserLogin.Models
         [Key]
         public int UserId { get; set; }
 
-        [Display(Name = "First Name")]
-        [Required(ErrorMessage = "First name cannot be empty")]
-        [MinLength(2, ErrorMessage = "First name is too short")]
-        public string FirstName { get; set; }
+        // [Display(Name = "First Name")]
+        // [Required(ErrorMessage = "First name cannot be empty")]
+        // [MinLength(2, ErrorMessage = "First name is too short")]
+        // public string FirstName { get; set; }
 
-        [Display(Name = "Last Name")]
-        [Required(ErrorMessage = "Last name cannot be empty")]
-        [MinLength(2, ErrorMessage = "Last name is too short")]
-        public string LastName { get; set; }
+        // [Display(Name = "Last Name")]
+        // [Required(ErrorMessage = "Last name cannot be empty")]
+        // [MinLength(2, ErrorMessage = "Last name is too short")]
+        // public string LastName { get; set; }
+
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username cannot be empty")]
+        [MinLength(2, ErrorMessage = "Username is too short")]
+        public string Username { get; set; }
 
         [EmailAddress]
         [Required]
@@ -28,6 +34,23 @@ namespace UserLogin.Models
         [MinLength(8, ErrorMessage = "Password must be 8 characters or longer!")]
         public string Password { get; set; }
 
+        [Display(Name = "Building Number")]
+        [Required]
+        public int StreetNumber {get; set;}
+
+        [Display(Name = "Street Name")]
+        [Required]
+        public string StreetName {get; set;}
+
+        [Required]
+        public string City {get; set;}
+
+        [Required]
+        public int Zipcode {get; set;}
+
+        [Display(Name = "Profile Picture")]
+        public string ProfilePic { get; set; }
+
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
@@ -36,5 +59,13 @@ namespace UserLogin.Models
         [Compare("Password")]
         [DataType(DataType.Password)]
         public string Confirm { get; set; }
+
+        // Relationships
+        List<User> Following { get; set; } // who the user is following M2M (other end of vvv)
+        List<User> Followers { get; set; } // who is following the user M2M (other end of ^^^)
+        List<GarageSale> Favorites { get; set; } // matches list of FavedBy in GarageSale M2M
+        List<GarageSale> GarageSales { get; set; } //matches Creator in GarageSale O2M
+        List<Review> Reviews { get; set; } // list of Reviews made O2M
+        List<Review> UserReviews { get; set; } // list of Reviews left O2M
     }
 }
