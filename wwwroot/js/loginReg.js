7b3c115c5735495c28a16e0a61ef07c22d79fe94
Luis Cardona <cardona-luis@outlook.com>
2021-02-22 15:02:54 -0800

    userAccount-done

--- wwwroot/js/loginReg.js
+++ wwwroot/js/loginReg.js
@@ -1,27 +1,44 @@
 // show the text and button options
-function step1UserInfo(e) {
-    document.getElementById("user-container").style.display = "grid";
-    document.getElementById("address-container").style.display = "none";
-    document.getElementById("userPhoto-container").style.display = "none";
+function Step1Reg(e) {
+    document.getElementById("section1Reg").style.display = "grid";
+    document.getElementById("section2Reg").style.display = "none";
+    document.getElementById("section3Reg").style.display = "none";
+    document.getElementById("section4Reg").style.display = "none";
+    document.getElementById("sectionHeading").innerText = "Customize your profile."
+    
 
 };
 
 
-const step2UserPhoto = () =>{
-    document.getElementById("user-container").style.display = "none";
-    document.getElementById("userPhoto-container").style.display = "grid";
-    document.getElementById("address-container").style.display = "none";
+const Step2Reg = () =>{
+    document.getElementById("section1Reg").style.display = "none";
+    document.getElementById("section2Reg").style.display = "grid";
+    document.getElementById("section3Reg").style.display = "none";
+    document.getElementById("section4Reg").style.display = "none";
+    document.getElementById("sectionHeading").innerText = "Create a password, login to your account in anytime."
 
 
 }
 
 
+const Step3Reg = () =>{
+    document.getElementById("section1Reg").style.display = "none";
+    document.getElementById("section2Reg").style.display = "none";
+    document.getElementById("section3Reg").style.display = "grid";
+    document.getElementById("section4Reg").style.display = "none";
+    document.getElementById("sectionHeading").innerText = "Customize your profile."
 
-function step3ZipCode(e) {
-    document.getElementById("user-container").style.display = "none";
-    document.getElementById("address-container").style.display = "none";
-    document.getElementById("userPhoto-container").style.display = "none";
-    document.getElementById("address-container").style.display = "grid";
+
+}
+
+
+function Step4Reg(e) {
+    document.getElementById("section1Reg").style.display = "none";
+    document.getElementById("section2Reg").style.display = "none";
+    document.getElementById("section3Reg").style.display = "none";
+    document.getElementById("section4Reg").style.display = "grid";
+    document.getElementById("sectionHeading").innerText = "Congratulation"
+
 
     
 };
